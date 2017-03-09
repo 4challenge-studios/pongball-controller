750abdd2db7938edde00e613d1d0f216ae8db057
Samuel Honorato <samuel.honorato.silva@gmail.com>
2017-03-09 09:24:18 -0300

    Retirada do botão de play/pause

--- pongball-controller/ViewController.swift
+++ pongball-controller/ViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController, MultiPeerDelegate {
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -28,8 +26,8 @@ class ViewController: UIViewController, MultiPeerDelegate {
     @IBOutlet weak var kickOutlet: UIButton!
     @IBOutlet weak var rightOutlet: UIButton!
     @IBOutlet weak var leftOutlet: UIButton!
+    
     func conectado(nome: String) {
-        
         DispatchQueue.main.async {
             self.conectado.text = nome
         }
@@ -54,11 +52,12 @@ class ViewController: UIViewController, MultiPeerDelegate {
     @IBAction func chute(_ sender: UIButton) {
         kickImageView.image = UIImage.init(named: "kick_\(cor)_up")
     }
-    
-    
     @IBAction func chuteDown(_ sender: UIButton) {
         enviaMensagem(comando: "kick")
         kickImageView.image = UIImage.init(named: "kick_\(cor)_down")
+    }
+    @IBAction func chuteExit(_ sender: UIButton) {
+        kickImageView.image = UIImage.init(named: "kick_\(cor)_up")
     }
     
     
@@ -66,55 +65,42 @@ class ViewController: UIViewController, MultiPeerDelegate {
         enviaMensagem(comando: "rightUp")
         rightImageView.image = UIImage.init(named: "right_\(cor)_up")
     }
-    
     @IBAction func direitaTouchDown(_ sender: UIButton) {
         enviaMensagem(comando: "rightDown")
         rightImageView.image = UIImage.init(named: "right_\(cor)_down")
-        
     }
+    @IBAction func direitaExit(_ sender: UIButton) {
+        enviaMensagem(comando: "rightUp")
+        rightImageView.image = UIImage.init(named: "right_\(cor)_up")
+    }
+    
     
     @IBAction func esquerda(_ sender: UIButton) {
         enviaMensagem(comando: "leftUp")
         leftImageView.image = UIImage.init(named: "left_\(cor)_up")
     }
-    
     @IBAction func leftTouchDown(_ sender: Any) {
         enviaMensagem(comando: "leftDown")
-        
         leftImageView.image = UIImage.init(named: "left_\(cor)_down")
-        
+    }
+    @IBAction func esquerdaExit(_ sender: UIButton) {
+        enviaMensagem(comando: "leftUp")
+        leftImageView.image = UIImage.init(named: "left_\(cor)_up")
     }
     
-    @IBAction func playpause(_ sender: UIButton) {
-        
-        if self.playPause {
-            enviaMensagem(comando: "play")
-            print("play")
-        }
-        else {
-            enviaMensagem(comando: "pause")
-            print("pause")
-        }
-
-        self.playPause = !self.playPause
-    }
     
     func enviaMensagem(comando: String) {
         let sessao = appDelegate.multipeer!.session
         
-        guard let master = self.appDelegate.multipeer?.displayNameMaster else {
-            return
-        }
-        
-        DispatchQueue.main.async {
-            do {
-                try sessao.send(comando.data(using: .utf8, allowLossyConversion: false)!, toPeers: [master], with: .reliable)
-            }catch _ {
-                
+        if let master = self.appDelegate.multipeer?.displayNameMaster {
+            DispatchQueue.main.async {
+                do {
+                    try sessao.send(comando.data(using: .utf8, allowLossyConversion: false)!, toPeers: [master], with: .reliable)
+                }catch _ {
+                    
+                }
             }
         }
+        
     }
-    
 }
-
-
